6335146146f84527b1e7f5e1714d2c07a74ecd6a
Joel Roggeman <joelroggeman@gmail.com>
2016-01-19 21:56:32 -0500

    Copy from dotfiles

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 mkdir -p $HOME/nvim
 
 # Linke init and all other files
-ln -sf ./vim/init.vim $HOME/nvim/init.vim
-ln -sf ./vim/general.vim $HOME/nvim/general.vim
-ln -sf ./vim/functions.vim $HOME/nvim/functions.vim
-ln -sf ./vim/keybindings.vim $HOME/nvim/keybindings.vim
-ln -sf ./vim/language.vim $HOME/nvim/language.vim
-ln -sf ./vim/plugins.vim $HOME/nvim/plugins.vim
-ln -sf ./vim/visual.vim $HOME/nvim/visual.vim
+ln -sf ~/.dotfiles/vim/init.vim $HOME/nvim/init.vim
+ln -sf ~/.dotfiles/vim/general.vim $HOME/nvim/general.vim
+ln -sf ~/.dotfiles/vim/functions.vim $HOME/nvim/functions.vim
+ln -sf ~/.dotfiles/vim/keybindings.vim $HOME/nvim/keybindings.vim
+ln -sf ~/.dotfiles/vim/language.vim $HOME/nvim/language.vim
+ln -sf ~/.dotfiles/vim/plugins.vim $HOME/nvim/plugins.vim
+ln -sf ~/.dotfiles/vim/visual.vim $HOME/nvim/visual.vim
