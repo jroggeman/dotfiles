8d14abf87742fb4e75d59d2bb75ef82afb94b8c7
Joel Roggeman <joelroggeman@gmail.com>
2016-01-19 22:06:02 -0500

    Merged in stashed changes to init

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
-# Make sure nvim directory exists
-mkdir -p $HOME/nvim
+# Link init and all other files
+ln -sf ~/.dotfiles/vim/init.vim ~/.vimrc
+ln -sf ~/.vimrc ~/nvim/init.vim
 
-# Linke init and all other files
-ln -sf ~/.dotfiles/vim/init.vim $HOME/nvim/init.vim
-ln -sf ~/.dotfiles/vim/general.vim $HOME/nvim/general.vim
-ln -sf ~/.dotfiles/vim/functions.vim $HOME/nvim/functions.vim
-ln -sf ~/.dotfiles/vim/keybindings.vim $HOME/nvim/keybindings.vim
-ln -sf ~/.dotfiles/vim/language.vim $HOME/nvim/language.vim
-ln -sf ~/.dotfiles/vim/plugins.vim $HOME/nvim/plugins.vim
-ln -sf ~/.dotfiles/vim/visual.vim $HOME/nvim/visual.vim
+ln -sf ~/.dotfiles/vim/general.vim ~/.vim/general.vim
+ln -sf ~/.dotfiles/vim/functions.vim ~/.vim/functions.vim
+ln -sf ~/.dotfiles/vim/keybindings.vim ~/.vim/keybindings.vim
+ln -sf ~/.dotfiles/vim/language.vim ~/.vim/language.vim
+ln -sf ~/.dotfiles/vim/plugins.vim ~/.vim/plugins.vim
+ln -sf ~/.dotfiles/vim/visual.vim ~/.vim/visual.vim
+
+ln -sf ~/.vim/ ~/nvim
